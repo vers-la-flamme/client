e4622775e23e7552c2c8816e995ff7bb7a983280
MrMouse <mrmousese@gmail.com>
2018-08-15 23:06:55 +0300

    -add: movement system

--- new3dRouglike/Assets/code/PlayerController.cs
+++ new3dRouglike/Assets/code/PlayerController.cs
@@ -1,13 +1,22 @@
-﻿using UnityEngine;
- 
+﻿
+using UnityEngine;
+
+
 public class PlayerController : MonoBehaviour
 {
+	public GameObject Target;
+	public GameObject Shell;
 	void Update()
 	{
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
 		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
  
-		transform.Rotate(0, x, 0);
-		transform.Translate(0, 0, z);
+		transform.Translate(-x, 0, 0);
+		transform.Translate(0, 0, -z);
+		
+		
+		Shell.transform.LookAt(Target.transform);
+		
+		
 	}
 }
