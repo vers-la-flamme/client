edf0c92de8d69ba79a97d4a3f38b80528e728ed7
MrMouse <mrmousese@gmail.com>
2018-08-16 19:37:39 +0300

    -replace project

---
+++ Assets/code/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+	public GameObject Target;
+	public GameObject Shell;
+	void Update()
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+ 
+		transform.Translate(-x, 0, 0);
+		transform.Translate(0, 0, -z);
+		
+		
+		Shell.transform.LookAt(Target.transform);
+		
+		
+	}
+}
---
+++ Assets/code/Target.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Target : MonoBehaviour
+{
+	public Camera playerCam;
+	
+	private RaycastHit hit;
+	
+	Ray MyRay;
+	
+	
+	public void Update()
+	{
+		MyRay = playerCam.ScreenPointToRay(Input.mousePosition);
+		Physics.Raycast(MyRay, out hit, Mathf.Infinity);
+		
+		Debug.DrawRay(MyRay.origin, MyRay.direction*10,Color.yellow);
+
+	
+		transform.position = new Vector3(hit.point.x,0.5f,hit.point.z);
+		Debug.Log(hit.point);
+		
+	
+
+
+
+
+	}
+}
+
+
